3f4d0924dc76c4a511d6996ba2ecd962379e9131
AutoUploader <uploader@bot.com>
2025-12-06 20:31:48 +0700

    Auto update Scripts/ModbyMlvnd.lua

---
+++ Scripts/ModbyMlvnd.lua
@@ -0,0 +1,62 @@
+--Craetedby.KamalMlvnd
+local Hui=GameMain:NewMod("ModbyMlvnd")
+function Hui:OnEnter()
+
+--13 SupremeEssenceCultivation
+--(Water)Sixteen Supreme Steps Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_1_Shui")
+--(Wood)Myriad Artifact Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_2_Mu")
+--(Metal)Seven-Slaughtering Sword Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_3_Jin")
+--(None)Jade Purity Immortal Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_4_None")
+--(Earth)Heaven Stealing Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_5_Tu")
+--(Fire)Primordial Alchemy Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_6_Huo")
+--(Fire)Emotion Severing Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_7_Huo")
+--(Metal)Grand Chariot Wisdom Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_8_Jin")
+--(Wood)Six Paths Reincarnation Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_9_Mu")
+--(Fire)True Sun Refining Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_10_Huo")
+--(Earth)Sunflower Refining Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_11_Tu")
+--(None)Primordial Symbols Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_12_None")
+--(None)Law of Nimbus Conquest Unlocked (DLC)
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_13_None")
+--(None)Unknown Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_DaNeng")
+--(None)Spirit Pet Practice Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_LingShou")
+--(None)Wrong Law Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_LOST")
+--(None)Law of Instinct Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Gong_YaoShou")
+
+--3 DivineEssenceCultivation
+--(God)Celestial Thunder Salvation Sutra Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("God_Gong_1")
+--(God)Eight Heavens Sadhana Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("God_Gong_2")
+--(God)Nether Salvation Sutra Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("God_Gong_3")
+
+--4 BodyEssenceCultivation
+--(Body)Primordial Unity Body Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Body_Gong_1")
+--(Body)Barbarous Forging Body Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Body_Gong_2")
+--(Body)Ancestral Yaoguai Body Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Body_Gong_3")
+--(Body)Fallen Obliteration Body Unlocked
+CS.XiaWorld.SchoolMgr.Instance:UnLockGong("Body_Gong_5")
+
+end
+
+
+
